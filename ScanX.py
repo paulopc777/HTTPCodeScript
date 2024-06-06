089e364153de52ab447189eee58678796f47596c
Paulo Cesar Granzieri <paulinho.pc@outlook.com>
2024-06-06 18:22:39 -0300

    Add Functions
    - Search for a single url
    - Save specific response codes
    - Verbose Mode

--- ScanX.py
+++ ScanX.py
@@ -29,7 +29,6 @@ def Text_Format(Domian:str):
     
     elif Domian.find("https://") == -1 and Domian.find("www.") >= 0 :
         FormaDomain = "https://" + Domian
-        
         # print(FormaDomain + "2")
         return FormaDomain
     elif Domian.find("https://") >= 0 and Domian.find("www.") == -1  :
@@ -50,45 +49,79 @@ def Get_Sattus_Code(Dominio:str):
             print(f"{Fore.GREEN} [+] {Fore.WHITE} - {Dominio} - HTTP OPEN - {Fore.GREEN}{Statuscode}")
             return Statuscode
         else:
-            print(f"{Fore.RED} [-] {Fore.WHITE} - {Dominio} - HTTP CLOSE - {Fore.RED}{Statuscode} ")
+            print(f"{Fore.YELLOW} [-] {Fore.WHITE} - {Dominio} - HTTP BLOCK - {Fore.YELLOW}{Statuscode}")
             return Statuscode
     except requests.exceptions.ConnectionError or requests.exceptions.ReadTimeout as e:
-        print(f"{Fore.RED} [-] {Fore.WHITE} - {Dominio} - {Fore.YELLOW}ERROR")
+            print(f"{Fore.RED} [x] {Fore.WHITE} - {Dominio} - NOT HTTP - {Fore.RED}CLOSE")
         
 def Create_Output(Array:list,name:str):
      with open(name, 'w') as arquivo:
            for Objeto in Array:
                arquivo.write(f"{Objeto}\n")
     
-def Start(ArquivoEntrada:str,ArquivoSaida:str):
+def SaveAndArquivo(ArquivoEntrada:str,ArquivoSaida:str,statusCode:int):
+    
     meu_array_200 = []
     
     try:
         
         with open(ArquivoEntrada , 'r') as arquivo: 
             for linha in arquivo:               
-                    Site = Text_Format(linha.strip())
-                    if Site == "":
-                        print(f"{Fore.YELLOW}start.py -h")
-                        print(f"{Fore.YELLOW}Input.txt:")
-                        print(f"{Fore.YELLOW}Website.com.br")
-                        print(f"{Fore.YELLOW}www.Site.com.br")
-                        print(f"{Fore.YELLOW}http://www.Site.com.br")
-                        print(f"{Fore.WHITE}")
-                    else:
-                        if Get_Sattus_Code(Site) == 200:
-                            meu_array_200.append(Site)
-                    
-        OutputFile = ArquivoSaida + ".txt"            
-        Create_Output(meu_array_200,OutputFile)
+                Site = Text_Format(linha.strip())
+                
+                if Site == "":
+                    print(f"{Fore.YELLOW}start.py -h")
+                    print(f"{Fore.YELLOW}Input.txt:")
+                    print(f"{Fore.YELLOW}Website.com.br")
+                    print(f"{Fore.YELLOW}www.Site.com.br")
+                    print(f"{Fore.YELLOW}http://www.Site.com.br")
+                    print(f"{Fore.WHITE}")
+                else:
+                    if Get_Sattus_Code(Site) == statusCode:
+                        meu_array_200.append(Site)
+                        
+        if ArquivoSaida:            
+            OutputFile = ArquivoSaida + ".txt"            
+            Create_Output(meu_array_200,OutputFile)
+        else:
+            return meu_array_200
+        
     except FileNotFoundError:
         print(f"{Fore.YELLOW}start.py -h")
         print(f"{Fore.YELLOW}Input File not Fund")
+
+def Onechar(Input:str,ArquivoSaida:str,statusCode:int):
+  
+    meu_array_200 = []
     
+    try:  
+        Site = Input
+        
+        if Site == "":
+            print(f"{Fore.YELLOW}start.py -h")
+            print(f"{Fore.YELLOW}Input.txt:")
+            print(f"{Fore.YELLOW}Website.com.br")
+            print(f"{Fore.YELLOW}www.Site.com.br")
+            print(f"{Fore.YELLOW}http://www.Site.com.br")
+            print(f"{Fore.WHITE}")
+        else:
+            if Get_Sattus_Code(Site) == statusCode:
+                meu_array_200.append(Site)
+            
+            if ArquivoSaida:            
+                OutputFile = ArquivoSaida + ".txt"            
+                Create_Output(meu_array_200,OutputFile)
+            else:
+                return meu_array_200
+        
+    except FileNotFoundError:
+        print(f"{Fore.YELLOW}start.py -h")
+        print(f"{Fore.YELLOW}Input File not Fund")
+
 def Prompt():
+    # Print Logos
     print(f"{Fore.YELLOW}\n")
     PrintLogo()
-    # print(f"{Fore.YELLOW}Uso: python start.py -o arquivo.txt")
     
     if len(sys.argv) <= 1:
         print(f"{Fore.YELLOW}start.py -h")
@@ -96,25 +129,40 @@ def Prompt():
         print(f"{Fore.WHITE}")
         sys.exit(1)
     
-    arg_1 = sys.argv[1]
-    if len(sys.argv) >= 4:
-        arg_2 = sys.argv[3]
-
-        if arg_1 == "-i" and arg_2 == "-o" and  len(sys.argv) >= 4:
-            nome_arquivo_entrada = sys.argv[2]
-            nome_arquivo_saida = sys.argv[4]
-            Start(nome_arquivo_entrada,nome_arquivo_saida)
-        else:
-            print("Argumento inválido ou nome do arquivo não fornecido corretamente.")
-    elif arg_1 == "-h":
-        print(f"{Fore.RED}TIPOS DE USO")
-        print(f"{Fore.GREEN}Uso: python start.py -i arquivoInput.txt -o arquivoOutput \n")
-        
-        print(f"{Fore.GREEN}Uso: python start.py -i arquivoInput.txt -o arquivoOutput -sv403")
-        print(f"{Fore.CYAN}Salva Codes = 403")
-    else:
-        print("Argumento inválido ou nome do arquivo não fornecido.")
+    #Numero de argumentos passados contando -i -o etc...
+    #Pedindo o Minimo que e -i = Input & -sv 
     
+    pedidominimo = 1
+    
+    if len(sys.argv) >= pedidominimo:
+        
+        # argumentos
+        Input=False
+        Output=False
+        Verbose=False
+        Save=200
+        
+        # Passando por todos os argumentos passados
+        for i in range(len(sys.argv)):
+            data = i + 1
+            if sys.argv[i] == "-i":
+                Input = sys.argv[data]
+            if sys.argv[i] == "-o":
+                Output = sys.argv[data]
+            if sys.argv[i] == "-v":
+                Verbose = True
+            if sys.argv[i] == "-sv":
+                Save=int(sys.argv[data])
+                
+                
+        if ".txt" in Input:
+            # Casso tenha passado um arquivo 
+            SaveAndArquivo(Input,Output,Save)
+        else:
+            SiteFormat = Text_Format(Input)
+            Onechar(SiteFormat,Output,Save)
+            # Casso não tenh apassado
+                
     print(f"{Fore.WHITE}")
     print("\n")
 
